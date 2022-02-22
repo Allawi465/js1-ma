279be92830757689138c02ba765467cd1617ae99
Allawi465 <allawi465@gmail.com>
2022-02-22 03:01:23 +0100

    .

---
+++ js/details.js
@@ -0,0 +1,46 @@
+const resultCotainer = document.querySelector(".details-container");
+
+const queryString = document.location.search;
+
+const params = new URLSearchParams(queryString);
+
+const id = params.get("id") -1;
+
+const idContainer = document.querySelector(".id");
+
+idContainer.innerHTML = id;
+
+console.log(id)
+
+const url = fetch("https://free-to-play-games-database.p.rapidapi.com/api/games", {
+    "headers": {
+       "x-rapidapi-key": "d252199cafmshb9ddfac0121f925p1d5962jsn8b7da9697655"
+    }
+});
+
+async function getGamesInfo() {
+    try {
+        const response = await url;
+
+        const data = await response.json();
+
+        console.log(data);
+
+        createHtml(data);
+        
+    } catch(error) {
+        console.log(error);
+
+        resultCotainer.innerHTML = `<p class="message"> An error occurred when calling the API</p>`
+    }
+};
+
+getGamesInfo()
+
+function createHtml(data) {
+    resultCotainer.innerHTML = `<h1 class="details-h1">${data[id].title}</h1>
+                                <div class="details-image2" 
+                                style="background-image: url('${data[id].thumbnail}')"></div>
+                                <div class="details-description">Description: ${data[id].short_description}</div>
+                                <p>Publisher: ${data[id].publisher}</p>`;
+}
